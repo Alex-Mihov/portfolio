d3cb6bd9e36e842bbb0ac58eed7f8fbd265cfafe
Alex-Mihov <alexmihov.as@gmail.com>
2025-08-26 20:00:36 +0200

    feat: update Footer component text size and enhance Header component layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <div className='h-fit w-[100%] bg-[#1e293b] flex justify-center items-center'>
-      <div className='pt-10 pb-10 text-[#7992a8] text-xl'>&copy; 2025 Alex Mihov. Creato con passione e tecnologia.</div>
+      <div className='pt-10 pb-10 text-[#7992a8] text-2xl'>&copy; 2025 Alex Mihov. Creato con passione e tecnologia.</div>
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,20 @@ const headerButtons = ["Home", "About", "Skills", "Projects", "Contact"];
 export default function Header() {
   return (
     <>
-    
-      <div className="flex justify-between items-center h-fit w-[100%] p-9">
-        <div className="text-3xl font-[750]">
+
+      <div className="flex justify-between items-center
+                      h-fit w-[100%] py-9 px-23 bg-[#1e293b]">
+        <div className="text-3xl font-[750] text-white">
           AM
         </div>
 
-        <div className="flex">
+        <div className="flex text-[#c5cedb] text-xl font-medium">
           {headerButtons.map((btn) => (
-            <div className="px-4" key={btn}>{btn}</div>
+            <div className="mx-4 hover:text-blue-600 
+                       transition-colors duration-300 ease-in-out
+                       cursor-pointer" key={btn}>
+              {btn}
+            </div>
           ))}
         </div>
       </div>
