cbd67a0b9c386044a370fee68f923b432316e2c4
Alex-Mihov <alexmihov.as@gmail.com>
2025-08-26 20:09:54 +0200

    feat: enhance Header component styles for improved interactivity

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,15 +6,18 @@ export default function Header() {
 
       <div className="flex justify-between items-center
                       h-fit w-[100%] py-9 px-23 bg-[#1e293b]">
-        <div className="text-3xl font-[750] text-white">
+        <div className="text-3xl font-[750] text-white 
+         hover:text-blue-700 
+          transition-colors duration-300 ease-in-out
+          cursor-pointer">
           AM
         </div>
 
         <div className="flex text-[#c5cedb] text-xl font-medium">
           {headerButtons.map((btn) => (
-            <div className="mx-4 hover:text-blue-600 
-                       transition-colors duration-300 ease-in-out
-                       cursor-pointer" key={btn}>
+            <div className="mx-4 hover:text-blue-700 
+               transition-colors duration-300 ease-in-out
+               cursor-pointer" key={btn}>
               {btn}
             </div>
           ))}
