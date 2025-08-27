627697f8dba883e29dd21b8b99d1e5e2dbed62bf
Alex-Mihov <alexmihov.as@gmail.com>
2025-08-27 16:29:49 +0200

    feat: enhance Header and Hero components with improved layout and interactivity

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,49 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-const headerButtons = [
-  { label: "Home", path: "/" },
-  { label: "About", path: "/aboutme" },
-  { label: "Skills", path: "/myskills" },
-  { label: "Projects", path: "/projects" },
-  { label: "Contact", path: "/contact" },
-];
+const headerButtons = ["Home", "About", "Skills", "Projects", "Contact"];
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <div className="flex justify-between items-center
-                      h-fit w-[100%] py-9 px-23 bg-[#0f172a]">
-        <Link to={"/"}
-          className="text-3xl font-[750] text-[#d3cdcd] 
-         hover:text-white 
-          transition-colors duration-300 ease-in-out
-          cursor-pointer">
+      <div
+        className={`fixed top-0 left-0 w-full flex justify-between items-center
+          py-9 px-23 transition-all duration-300 ease-in-out z-50
+          ${scrolled
+            ? "bg-[#0f172a]/60 border-b border-gray-500 backdrop-blur-md"
+            : "bg-[#0f172a] border-b-0"
+          }`}
+      >
+        <Link
+          to={"/"}
+          className="text-3xl font-[750] text-[#d3cdcd] hover:text-white transition-colors duration-300 ease-in-out cursor-pointer"
+        >
           AM
         </Link>
 
         <div className="flex text-[#d3cdcd] text-xl font-medium">
           {headerButtons.map((btn) => (
-            <Link
-              to={btn.path}
-              className="mx-4 hover:text-white 
-               transition-colors duration-300 ease-in-out
-               cursor-pointer"
-              key={btn.label}
+            <div
+              className="mx-4 hover:text-white transition-colors duration-300 ease-in-out cursor-pointer"
+              key={btn}
             >
-              {btn.label}
-            </Link>
+              {btn}
+            </div>
           ))}
         </div>
       </div>
+      <div />
     </>
   );
 }
